7fa6d06db57f9931913300d1407b9625c53350d4
Rogisimo <robertasmurnikovas1@gmail.com>
2022-08-24 19:35:52 +0300

    Sorting HTL, LTH, Rating + rating stars via script

--- books.js
+++ books.js
@@ -1,25 +1,33 @@
 
 
-function renderBooks(){
+function renderBooks(filter){
   const booksWrapper = document.querySelector('.books');
+
   const books = getBooks();
+
+  if(filter === "LOW_TO_HIGH"){
+    books.sort((a, b) => a.originalPrice - b.originalPrice);
+  }
+  else if(filter === "HIGH_TO_LOW"){
+    books.sort((a, b) => b.originalPrice - a.originalPrice);  
+  }
+  else if(filter === "RATING"){
+    books.sort((a, b) => b.rating - a.rating);
+  }
+
   const booksHtml = books.map(book => {
     return `<div class="book">
   <figure class="book_img-wrapper">
     <img class="book_img" src="${book.url}" alt="">
   </figure>
   <div class="book_title">
-    ${books[0].title}
+    ${book.title}
   </div>
   <div class="book_readings">
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star-half-alt"></i>
+    ${ratingsHTML(book.rating)}
   </div>
   <div class="book_price">
-    <span class="book_price-normal">$${books[0].originalPrice}</span> $${books[0].salePrice}
+    <span class="book_price-normal">$${book.originalPrice}</span> $${book.salePrice}
   </div>
 </div>`
   }).join("")
@@ -30,6 +38,23 @@ function renderBooks(){
 setTimeout(() => {
   renderBooks()
 });
+
+function ratingsHTML(rating){
+  let ratingHTML = '';
+
+  for(let i = 0; i < Math.floor(rating); i++){
+    ratingHTML += '<i class="fas fa-star"></i>'
+  }
+  if(!Number.isInteger(rating)){
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>'
+  }
+
+  return ratingHTML
+}
+
+function filterBooks(event){
+    renderBooks(event.target.value);
+}
 
 // FAKE DATA
 function getBooks() {
